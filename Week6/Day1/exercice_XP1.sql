cd50da994df27e302fc5969ffad4cfac2d3d1db0
Davy <d.sebbah@yahoo.fr>
2021-11-07 17:34:47 +0200

    first exercice SQL

---
+++ Week6/Day1/exercice_XP1.sql
@@ -0,0 +1,21 @@
+
+-- All the items.
+SELECT * FROM items;
+
+-- All the items with a price above 80 (80 not included).
+-- SELECT * FROM items WHERE (small_desk>2000 OR large_desk>2000 OR fan>80);
+
+-- All the items with a price below 300. (300 included)
+
+
+-- SELECT * FROM customers;
+-- All customers whose last name is ‘Smith’ (What will be your outcome?).
+-- SELECT first_name, last_name FROM customers WHERE last_name='Smith'
+
+
+-- All customers whose last name is ‘Jones’.
+-- SELECT * FROM customers WHERE last_name='Jones';
+
+
+-- All customers whose firstname is not ‘Scott’.
+-- SELECT * FROM customers WHERE first_name!='Scott';
