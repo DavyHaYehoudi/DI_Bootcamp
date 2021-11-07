1942f0b6e7b17465c1017e87938709b1247cf687
Davy <d.sebbah@yahoo.fr>
2021-11-07 19:31:31 +0200

    dailychallenge

---
+++ Week6/Day1/DailyChallenge.sql
@@ -0,0 +1,7 @@
+SELECT * FROM actors;
+
+--Count how many actors are in the table.
+--SELECT count (actor_id) AS number_actor FROM actors;
+
+-- Try to add a new actor with some blank fields. What do you think the outcome will be ?
+--An error because we have defined 'NOT NULL'
